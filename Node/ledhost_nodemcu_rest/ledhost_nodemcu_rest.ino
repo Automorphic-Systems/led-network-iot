8466205ab1070bffee46b5e5f6c017a7b17c5a88
Matt Brooks <lessthanepsilon@gmail.com>
2021-04-19 22:49:56 -0600

    updating mcu/rest tests

--- Node/ledhost_nodemcu_rest/ledhost_nodemcu_rest.ino
+++ Node/ledhost_nodemcu_rest/ledhost_nodemcu_rest.ino
@@ -1,4 +1,4 @@
-#include <ESP8266WiFi.h>
+d#include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <FastLED.h>
@@ -7,10 +7,10 @@
 
 #define LED_PIN 7
 #define UCHAR_MAX 255
-#define NUM_LEDS 114
+#define NUM_LEDS 288
 #define DEFAULT_PORT 8000
 #define DEFAULT_INTERVAL 32
-#define DEFAULT_BRIGHTNESS 224
+#define DEFAULT_BRIGHTNESS 192
 #define DEFAULT_WIFI_TIMEOUT 20000
 #define LEDHOST_CONFIG "/ledhost_config.json"
 
@@ -391,7 +391,6 @@ void handleMode() {
 }
 
 void handleFrame() {
-  
     server.send(200, "text/plain","{ result: 1 }");
 }
 
